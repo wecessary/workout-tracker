6d3cb1d8bce93e2fd79007b10494d4dc3e209cf6
wecesscary <wesleymhj@gmail.com>
2022-09-26 15:32:06 +0100

    added one simple unit test

--- src/pages/TrackerPage.test.tsx
+++ src/pages/TrackerPage.test.tsx
@@ -2,9 +2,21 @@
  * @jest-environment jsdom
  */
 
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom"
+import userEvent from "@testing-library/user-event"
+import { render, screen } from "@testing-library/react";
 import TrackerPage from "./TrackerPage";
 
-test("hello jest", () => {
+describe("Tracker Page", () => {
   render(<TrackerPage />);
+
+  test("can add set", async () => {
+    const user = userEvent.setup()
+    const addBtn = await screen.findByRole("button",{name: /add exercise/i})
+    await user.click(addBtn)
+    const exerciseNameBoxes = await screen.findAllByDisplayValue(/Your workout/i)
+    expect(exerciseNameBoxes).toHaveLength(1)
+  });
 });
+
+
