4103c8561c8125b841ab5f23bb860e7731ae3159
wecesscary <wesleymhj@gmail.com>
2022-10-14 15:37:39 +0100

    make feedback message for saving sticky

--- src/components/NotificationChip.tsx
+++ src/components/NotificationChip.tsx
@@ -2,26 +2,35 @@ import { ReactNode, useEffect, useState } from "react";
 
 const NotificationChip = ({
   children,
-  clearStatus,
+  resetStatus,
+  statuses,
 }: {
   children: ReactNode;
-  clearStatus: () => void;
+  resetStatus: () => void;
+  statuses: boolean[];
 }) => {
   useEffect(() => {
     const timeId = setTimeout(() => {
       // After 3 seconds set the show value to false
-      clearStatus();
+      resetStatus();
     }, 3000);
 
     return () => {
       clearTimeout(timeId);
+      // The return is a clean up function.
+      // React cleans up when the component unmounts and cleans up the previous effect before running the effect next time
+      // With this cleanup function, it means (an example):
+      // 1. if a user clicks "save data", timer1 starts. When the timer ends, isSaving and hasSaved will be set to false. This would
+      // 2. cause the component to unmount.
+      // 2. if a user clicks again right away, a second timer starts, the clean up function cancels timer 1 and
+      // stops calling isSaving and hasSaved from being set to false three seconds from when the first timer was called
     };
-  }, []);
+  }, [...statuses]);
 
   return (
-    <div className="flex gap-4 p-4 shadow-md rounded-md">
+    <div className="flex gap-4 p-4 shadow-md rounded-md sticky bottom-2 bg-white text-slate-400">
       {children}
-      <button onClick={clearStatus}>X</button>
+      <button onClick={resetStatus}>X</button>
     </div>
   );
 };
--- src/components/ProtectedContentLayout.tsx
+++ src/components/ProtectedContentLayout.tsx
@@ -7,19 +7,19 @@ import ProtectedRoutes from "../routeProtection/ProtectedRoutes";
 
 const Layout = () => {
   const { user } = useContext(AuthContext);
-  const username = user ? user.displayName : "Person With No Name"
+  const username = user ? user.displayName : "Person With No Name";
 
   return (
     <>
       <div className="flex flex-col justify-center items-center p-4">
         <h1> {`${username}'s Workout Tracker💪`}</h1>
-      <ProtectedRoutes>
-        <UserDataContextProvider>
-          <CheckDataIsLoaded>
-            <Outlet />
-          </CheckDataIsLoaded>
-        </UserDataContextProvider>
-      </ProtectedRoutes>
+        <ProtectedRoutes>
+          <UserDataContextProvider>
+            <CheckDataIsLoaded>
+              <Outlet />
+            </CheckDataIsLoaded>
+          </UserDataContextProvider>
+        </ProtectedRoutes>
       </div>
     </>
   );
