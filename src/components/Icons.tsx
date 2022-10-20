ac08d83d43f3518c1098dc986b1b1d92d0960c84
wecessary <92378671+wecessary@users.noreply.github.com>
2022-10-20 18:44:56 +0100

    add toggle option to delete sets and exercise (#23)

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -33,3 +33,22 @@ export const NoSymbol = () => {
     </svg>
   );
 };
+
+export const Minus = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6 text-blue-800"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+};
