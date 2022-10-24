4eda3a26b6d3915e8752c200ac7cb91f8b5a8d88
wecessary <92378671+wecessary@users.noreply.github.com>
2022-10-24 18:41:40 +0100

    add onclickoustide will disable popup (#26)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProtectedContentLayout from "./components/ProtectedContentLayout";
 import AuthContextProvider from "./context/AuthContext";
-import Dnd from "./pages/DnD";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import TrackerPage from "./pages/TrackerPage";
-import { DragDropContext } from "react-beautiful-dnd";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
@@ -17,7 +15,6 @@ const App = () => {
       <AuthContextProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/dnd" element={<Dnd />}></Route>
             <Route path="/" element={<LoginPage />}></Route>
             <Route path="/register" element={<RegisterPage />}></Route>
             <Route element={<ProtectedContentLayout />}>
---
+++ src/hooks/useOutsideClick.tsx
@@ -0,0 +1,20 @@
+import { MutableRefObject, useEffect, useRef } from "react";
+
+const useOutsideClick = (callback: () => void) => {
+  const ref: MutableRefObject<null | HTMLButtonElement> = useRef(null);
+
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, [ref]);
+
+  return ref;
+};
+
+export default useOutsideClick;
