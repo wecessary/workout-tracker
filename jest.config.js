477c0c1d57d2f40a8920bcf524b918031b3dd98e
wecessary <92378671+wecessary@users.noreply.github.com>
2022-10-27 12:56:26 +0100

    Add unit tests on adding sets exercises (#33)
    * add unit tests on adding sets and exercises

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('jest').Config} */
+const config = {
+  resolver: "<rootDir>/jest.resolver.js",
+};
+
+module.exports = config;
--- src/pages/TrackerPage.test.tsx
+++ src/pages/TrackerPage.test.tsx
@@ -2,21 +2,37 @@
  * @jest-environment jsdom
  */
 
-import "@testing-library/jest-dom"
-import userEvent from "@testing-library/user-event"
-import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import { render, screen, waitFor } from "@testing-library/react";
 import TrackerPage from "./TrackerPage";
 
-describe("Tracker Page", () => {
-  render(<TrackerPage />);
+function setup(jsx: JSX.Element) {
+  return {
+    user: userEvent.setup(),
+    ...render(jsx),
+  };
+}
 
-  test("can add set", async () => {
-    const user = userEvent.setup()
-    const addBtn = await screen.findByRole("button",{name: /add exercise/i})
-    await user.click(addBtn)
-    const exerciseNameBoxes = await screen.findAllByDisplayValue(/Your workout/i)
-    expect(exerciseNameBoxes).toHaveLength(1)
+describe("Tracker Page", () => {
+  test("can add sets", async () => {
+    const { user } = setup(<TrackerPage />);
+    const setLables = await screen.findAllByText(/set/i);
+    expect(setLables).toHaveLength(1);
+
+    const addSetBtn = await screen.findByRole("button", { name: "+" });
+    await user.click(addSetBtn);
+    const upDatedSetLables = await screen.findAllByText(/set/i);
+    await waitFor(() => expect(upDatedSetLables).toHaveLength(2));
+  });
+
+  test("can add exercise", async () => {
+    const { user } = setup(<TrackerPage />);
+    const addBtn = await screen.findByRole("button", { name: /add exercise/i });
+    await user.click(addBtn);
+    const exerciseNameBoxes = await screen.findAllByPlaceholderText(
+      /exercise name/i
+    );
+    expect(exerciseNameBoxes).toHaveLength(2);
   });
 });
-
-
