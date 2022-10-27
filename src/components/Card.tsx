ca340bc01e1a51a6d9f71ac68c715e8701ac6967
wecesscary <wesleymhj@gmail.com>
2022-10-27 16:40:00 +0100

    change background color

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Card = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="group p-6 max-w-[310px] my-2 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+    <div className="group p-6 max-w-[310px] my-2 bg-app-yellow-100 rounded-lg shadow-lg hover:bg-app-yellow-500">
       {children}
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "app-yellow": {
+          100: "#fff3ca",
+          500: "#ffca43",
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
