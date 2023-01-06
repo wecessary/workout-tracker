18d59007148f4de8e6b877c930b88f21a85a0d80
wecesscary <wesleymhj@gmail.com>
2023-01-06 14:26:05 +0000

    change delete set icon and add animation

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -278,7 +278,7 @@ export const ThreeDots = () => (
   </svg>
 );
 
-export const BackSpace = () => (
+export const XCircle = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
@@ -290,7 +290,7 @@ export const BackSpace = () => (
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z"
+      d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
     />
   </svg>
 );
