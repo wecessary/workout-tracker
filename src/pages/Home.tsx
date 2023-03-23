1b399ebf9d5b173fd3995f9faed120119ee2cc90
wecesscary <wesleymhj@gmail.com>
2023-03-23 13:26:24 +0000

    feat: home animation and catch google error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,30 @@
+import { animated, config, useSpring } from "@react-spring/web";
 import { Link } from "react-router-dom";
 import { AppIcon } from "../components/ui/Icons";
 
 const Index = () => {
+  const [spring, api] = useSpring(() => ({
+    from: { y: 1000 },
+    to: { y: 0 },
+    config: config.slow,
+  }));
+
   return (
     <>
       <div className="flex justify-center items-center h-screen w-screen bg-mobile-bg md:bg-desktop-bg bg-cover bg-center">
         <div className="h-[85vh] flex flex-col justify-between">
-          <div>
-            <AppIcon />
-            <h2 className="mt-4 border-t-4 border-spacing-y-2 font-bold text-white ">
-              no-nonsense workout tracker
-            </h2>
+          <div className="overflow-hidden">
+            <animated.div style={{ ...spring }}>
+              <AppIcon />
+              <h2 className="mt-4 border-t-4 border-spacing-y-2 font-bold text-white ">
+                no-nonsense workout tracker
+              </h2>
+            </animated.div>
           </div>
 
           <Link
             to={"/login"}
-            className=" text-center border-2 hover:border-4 border-white p-4 rounded-xl w-full text-white"
+            className="text-center border-2 hover:border-4 border-white p-4 rounded-xl w-full text-white"
           >
             Use for free
           </Link>
