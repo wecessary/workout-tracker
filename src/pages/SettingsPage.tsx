2a0d05a7d5e5ded6ae7ae2ac60719c53c3dc2198
wecesscary <wesleymhj@gmail.com>
2023-03-24 18:12:56 +0000

    restyle: settings page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -3,8 +3,10 @@ import { logOut } from "../lib/firebase";
 
 const Settings = () => {
   return (
-    <div>
-      <Button onClick={logOut}>Log out</Button>
+    <div className="flex h-screen justify-center items-center">
+      <Button variant="outline" onClick={logOut}>
+        Log out
+      </Button>
     </div>
   );
 };
